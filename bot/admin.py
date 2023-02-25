2c68e8984d7cd63f556b747579cda987311efc2b
gmoroz <12345hasek@gmail.com>
2023-02-25 20:58:12 +0300

    feat: Add base models and admin registration for Category, Subcategory, and Product
    Added base model BaseModel with name attribute, which is inherited by Category, Subcategory, and Product models. Category and Subcategory have a one-to-many relationship. Product has a foreign key to Subcategory.
    
    Also added admin registration for Category, Product, and Subcategory models in admin.py.

--- bot/admin.py
+++ bot/admin.py
@@ -1,2 +1,6 @@
+from django.contrib import admin
+from .models import Category, Product, Subcategory
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Subcategory)
--- bot/models.py
+++ bot/models.py
@@ -1,2 +1,26 @@
+from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    pass
+
+
+class Subcategory(BaseModel):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="subcategories"
+    )
+
+
+class Product(BaseModel):
+    description = models.TextField()
+    image = models.ImageField()
+    subcategory = models.ForeignKey(
+        Subcategory, on_delete=models.CASCADE, related_name="products"
+    )
