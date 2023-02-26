dce035039ac414e33320fdd9c430589c0877529f
gmoroz <12345hasek@gmail.com>
2023-02-26 11:16:58 +0300

    perf: rename func

--- bot/tg_bot/utils/db_queries.py
+++ bot/tg_bot/utils/db_queries.py
@@ -3,7 +3,7 @@ from asgiref.sync import sync_to_async
 from django.db.models.query import QuerySet
 
 
-async def paginate(page: int, page_size: int, qs: QuerySet) -> QuerySet:
+async def paginate_qs(page: int, page_size: int, qs: QuerySet) -> QuerySet:
     """Разбивает кверисет на страницы."""
     offset = (page - 1) * page_size
     limit = page_size
