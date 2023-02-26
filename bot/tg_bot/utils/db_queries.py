81b4c4c7d8a9b581a4b4fe15a108598c7f2201e3
gmoroz <12345hasek@gmail.com>
2023-02-26 08:49:33 +0300

    feat: Add db_queries.py
    This commit adds a new file "db_queries.py" which contains functions for executing database queries. This feature will allow for easier and more efficient interaction with the database, improving the overall performance of the system.

---
+++ bot/tg_bot/utils/db_queries.py
@@ -0,0 +1,15 @@
+from bot.models import Category
+from asgiref.sync import sync_to_async
+from django.db.models.query import QuerySet
+
+
+async def paginate(page: int, page_size: int, qs: QuerySet) -> QuerySet:
+    """Разбивает кверисет на страницы."""
+    offset = (page - 1) * page_size
+    limit = page_size
+    return qs[offset : offset + limit]
+
+
+async def get_categories() -> QuerySet:
+    """Получает список категорий из базы данных."""
+    return await sync_to_async(Category.objects.order_by)("id")
