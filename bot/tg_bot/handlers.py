e3d4a88776b6010a3f1f8e0b0b95db2a3b218a48
gmoroz <12345hasek@gmail.com>
2023-02-26 04:46:51 +0300

    feat: Add menu_handler to start_handler in handlers.py
    Added menu_handler to start_handler in handlers.py to provide users with options to choose from after checking their subscription status. InlineKeyboardMarkup is used to create a menu with three buttons: "Catalog", "Cart", and "FAQ". If the user is subscribed, the menu is displayed, otherwise a message is sent saying "You are not subscribed to the channel!".

--- bot/tg_bot/handlers.py
+++ bot/tg_bot/handlers.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message
 from bot.tg_bot.channels import check_subscription
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def start_handler(message: Message):
@@ -9,5 +10,19 @@ async def start_handler(message: Message):
 
     if is_subscribed:
         await message.answer("Вы уже подписаны на канал!")
+    else:
+        await message.answer("Вы не подписаны на канал!")
+
+
+async def menu_handler(message: Message):
+    if await check_subscription(message.from_user.id):
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard.add(
+            InlineKeyboardButton("Каталог", callback_data="catalog"),
+            InlineKeyboardButton("Корзина", callback_data="cart"),
+            InlineKeyboardButton("FAQ", callback_data="faq"),
+        )
+        # Отправляем сообщение с InlineKeyboardMarkup пользователю
+        await message.answer("Выберите действие:", reply_markup=keyboard)
     else:
         await message.answer("Вы не подписаны на канал!")
--- start_bot.py
+++ start_bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 from aiogram.utils import executor
-from bot.tg_bot.handlers import start_handler
+from bot.tg_bot.handlers import menu_handler, start_handler
 
 # настройка Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bottec_test.settings")
@@ -22,7 +22,7 @@ dp = Dispatcher(bot, storage=storage)
 # регистрация хэндлеров и настроек диспетчера
 
 dp.register_message_handler(start_handler, Command("start"))
-
+dp.register_message_handler(menu_handler, Command("menu"))
 # запуск бота
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
