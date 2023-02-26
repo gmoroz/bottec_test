015b07b28940a25d9f5754bfdb232536ae0df05b
gmoroz <12345hasek@gmail.com>
2023-02-26 08:45:27 +0300

    feat: Add catalog functionality to bot menu
     This commit adds new functionality to the bot's menu by registering new handlers for catalog queries. The menu_handler now includes a new option for the user to access the catalog, which is handled by catalog_handler. Additionally, the new menu_callback_handler function allows the user to return to the main menu after navigating to the catalog. This change also includes some minor refactoring of the existing code.

--- bot/tg_bot/handlers/main.py
+++ bot/tg_bot/handlers/main.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message
 from bot.tg_bot.channels import check_subscription
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
 async def menu_handler(message: Message):
@@ -8,9 +8,13 @@ async def menu_handler(message: Message):
     if await check_subscription(message):
         keyboard = InlineKeyboardMarkup(row_width=2)
         keyboard.add(
-            InlineKeyboardButton("Каталог", callback_data="catalog"),
+            InlineKeyboardButton("Каталог", callback_data="catalog:1"),
             InlineKeyboardButton("Корзина", callback_data="cart"),
             InlineKeyboardButton("FAQ", callback_data="faq"),
         )
         # Отправляем сообщение с InlineKeyboardMarkup пользователю
         await message.answer("Выберите действие:", reply_markup=keyboard)
+
+
+async def menu_callback_handler(query: CallbackQuery):
+    await menu_handler(query.message)
--- start_bot.py
+++ start_bot.py
@@ -5,7 +5,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 from aiogram.utils import executor
-from bot.tg_bot.handlers.main import menu_handler
+
+from bot.tg_bot.handlers.main import menu_callback_handler, menu_handler
 
 # настройка Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bottec_test.settings")
@@ -13,6 +14,7 @@ django.setup()
 
 
 from django.conf import settings
+from bot.tg_bot.handlers.categories import catalog_callback_handler, catalog_handler
 
 # инициализация бота и диспетчера
 bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -22,6 +24,11 @@ dp = Dispatcher(bot, storage=storage)
 # регистрация хэндлеров и настроек диспетчера
 
 dp.register_message_handler(menu_handler, Command(["menu", "start"]))
+dp.register_callback_query_handler(menu_callback_handler, lambda c: c.data == "menu")
+dp.register_callback_query_handler(catalog_handler, text="catalog")
+dp.register_callback_query_handler(
+    catalog_callback_handler, lambda query: query.data.startswith("catalog:")
+)
 
 # запуск бота
 if __name__ == "__main__":
