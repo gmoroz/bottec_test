867a3e558f7291f7df4bc65273390f05896b901e
gmoroz <12345hasek@gmail.com>
2023-02-27 09:50:38 +0300

    fix: Fix a bug in product_callback_handler function

--- bot/tg_bot/handlers/products.py
+++ bot/tg_bot/handlers/products.py
@@ -1,10 +1,14 @@
-from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import (
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+)
 from django.conf import settings
-from bot.tg_bot.utils.db_queries import paginate_qs
 from bot.models import Subcategory
 from aiogram.dispatcher import FSMContext
-
+from asgiref.sync import sync_to_async
 from bot.tg_bot.utils.paginate import get_buttons
+from aiogram.types.input_file import InputFile
 
 
 async def product_callback_handler(query: CallbackQuery, state: FSMContext):
@@ -15,11 +19,33 @@ async def product_callback_handler(query: CallbackQuery, state: FSMContext):
 async def products_handler(query: CallbackQuery, page: int = 1):
     subcategory_id = int(query.data.split(":")[1])
     subcategory = await Subcategory.objects.aget(pk=subcategory_id)
-    products = subcategory.products.all()
-    products_qs = await paginate_qs(page, settings.PRODUCTS_PAGE_SIZE, products)
+    products = subcategory.products.order_by("pk").all()
 
-    InlineKeyboardMarkup(row_width=2)
-    async for product in products_qs:
-        InlineKeyboardButton()
-    total_pages = settings.CATEGORY_PAGE_SIZE
-    await get_buttons(page, total_pages, callback_data=f"cart:{product.id}:1")
+    keyboard = InlineKeyboardMarkup(row_width=2)
+
+    products_count = await sync_to_async(products.count)()
+    total_pages = (
+        products_count + int(settings.PRODUCTS_ITEMS_ON_PAGE > 1)
+    ) // settings.PRODUCTS_ITEMS_ON_PAGE
+    product = await sync_to_async(products.__getitem__)(page - 1)
+    butttons = await get_buttons(
+        page, total_pages, callback_data=f"product:{subcategory_id}"
+    )
+    keyboard.add(*butttons)
+    if query.message.photo:
+        await query.bot.edit_message_media(
+            chat_id=query.message.chat.id,
+            message_id=query.message.message_id,
+            media=InputMediaPhoto(
+                media=InputFile(product.image.path),
+                caption=await product.caption(),
+            ),
+            reply_markup=keyboard,
+        )
+    else:
+        await query.bot.send_photo(
+            chat_id=query.message.chat.id,
+            photo=InputFile(product.image.path),
+            caption=await product.caption(),
+            reply_markup=keyboard,
+        )
