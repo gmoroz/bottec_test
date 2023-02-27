aa980dfe9372fad89a01b0c52816763942aa3807
gmoroz <12345hasek@gmail.com>
2023-02-27 13:46:11 +0300

    feat: Update check_subscription in channels.py
    This commit updates the check_subscription function in channels.py to save the user's Telegram ID and username to the database upon successful subscription to the channel.

--- bot/tg_bot/channels.py
+++ bot/tg_bot/channels.py
@@ -2,6 +2,8 @@ from aiogram.utils.exceptions import TelegramAPIError
 from aiogram.types import Message
 from django.conf import settings
 
+from bot.models import User
+
 
 async def check_subscription(message: Message):
     from start_bot import bot
@@ -10,6 +12,10 @@ async def check_subscription(message: Message):
         chat_member = await bot.get_chat_member(
             chat_id=settings.CHANNEL_ID, user_id=message.from_user.id
         )
-        return chat_member.is_chat_member()
+        tg_id = message.from_user.id
+        username = message.from_user.username
+        if chat_member.is_chat_member():
+            await User.objects.aget_or_create(username=username, tg_id=tg_id)
+            return True
     except TelegramAPIError:
         await message.answer("Вы не подписаны на канал {settings.CHANNEL_ID}")
