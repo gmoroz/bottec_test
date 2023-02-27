da6385930d0f124034a2e29df731be39a062fdf0
gmoroz <12345hasek@gmail.com>
2023-02-27 13:45:57 +0300

    feat: Add models and update admin for Cart and CartProduct

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Category, Product, Subcategory
-from .models import User
+from .models import Category, Product, Subcategory, User, Cart, CartProduct
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Subcategory)
 admin.site.register(User)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
