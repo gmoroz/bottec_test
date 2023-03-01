0c874b602ec81c9a888803d71e884d35cf9784a9
gmoroz <12345hasek@gmail.com>
2023-03-01 20:10:29 +0300

    fix: error when start bot

--- bot/tg_bot/handlers/main.py
+++ bot/tg_bot/handlers/main.py
@@ -5,16 +5,15 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQu
 
 async def menu_handler(message: Message, check=True):
     """Хэндлер для обработки команды /menu и /start. Выводит меню, проверяет подписку"""
-    if check:
-        await check_subscription(message)
-    keyboard = InlineKeyboardMarkup(row_width=2)
-    keyboard.add(
-        InlineKeyboardButton("Каталог", callback_data="catalog:1"),
-        InlineKeyboardButton("Корзина", callback_data="cart:1"),
-        InlineKeyboardButton("FAQ", callback_data="faq:1"),
-    )
-    # Отправляем сообщение с InlineKeyboardMarkup пользователю
-    await message.answer("Выберите действие:", reply_markup=keyboard)
+    if await check_subscription(message):
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard.add(
+            InlineKeyboardButton("Каталог", callback_data="catalog:1"),
+            InlineKeyboardButton("Корзина", callback_data="cart:1"),
+            InlineKeyboardButton("FAQ", callback_data="faq:1"),
+        )
+        # Отправляем сообщение с InlineKeyboardMarkup пользователю
+        await message.answer("Выберите действие:", reply_markup=keyboard)
 
 
 async def menu_callback_handler(query: CallbackQuery):
